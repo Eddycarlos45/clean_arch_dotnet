40a705f72c76c8c03bba596940891c2aa4cc4fa5
Edson Sousa <edsoncarlos33@hotmail.com>
2025-01-20 18:57:09 -0300

    feat: add category repository

---
+++ CleanArchMVC/CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class CategoryRepository: ICategoryRepository
+    {
+        ApplicationDbContext _categorycontext;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categorycontext = context;
+        }
+
+        public async Task<Category> CreateAsync(Category category)
+        {
+            _categorycontext.Add(category);
+            await _categorycontext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetByIdAsync(int? id)
+        {
+            return await _categorycontext.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategoriesAsync()
+        {
+            return await _categorycontext.Categories.ToListAsync();
+        }
+
+        public async Task<Category> RemoveAsync(Category category)
+        {
+            _categorycontext.Remove(category);
+            await _categorycontext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> UpdateAsync(Category category)
+        {
+            _categorycontext.Update(category);
+            await _categorycontext.SaveChangesAsync();
+            return category;
+        }
+    }
+}
