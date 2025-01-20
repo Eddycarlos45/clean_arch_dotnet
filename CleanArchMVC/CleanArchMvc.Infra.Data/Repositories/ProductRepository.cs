50405c27c2fe3bd71547edf16a96074021de881d
Edson Sousa <edsoncarlos33@hotmail.com>
2025-01-20 19:08:58 -0300

    feat: add product repository

---
+++ CleanArchMVC/CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,53 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+        ApplicationDbContext _productcontext;
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productcontext = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _productcontext.Add(product);
+            await _productcontext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetByIdAsync(int? id)
+        {
+            return await _productcontext.Products.FindAsync(id);
+        }
+
+        public async Task<Product> GetProductCategoryAsync(int? id)
+        {
+            return await _productcontext.Products.Include(c => c.Category)
+                .SingleOrDefaultAsync(p=>p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _productcontext.Products.ToListAsync();
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _productcontext.Remove(product);
+            await _productcontext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _productcontext.Update(product);
+            await _productcontext.SaveChangesAsync();
+            return product;
+        }
+    }
+}
