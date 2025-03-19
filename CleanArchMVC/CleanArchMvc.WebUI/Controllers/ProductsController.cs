f03fb19d2f45e13bb8968f76294834c6baf154a2
Edson Sousa <edsoncarlos33@hotmail.com>
2025-03-19 16:02:33 -0300

    feat:create products controller

---
+++ CleanArchMVC/CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var procucts = await _productService.GetProducts();
+            return View(procucts);
+        }
+    }
+}
