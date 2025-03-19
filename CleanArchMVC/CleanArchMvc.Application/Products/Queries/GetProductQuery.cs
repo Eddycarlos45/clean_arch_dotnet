87bc3cf9427316e5c602dae7302c7a6773d58fa3
Edson Sousa <edsoncarlos33@hotmail.com>
2025-03-19 16:37:16 -0300

    feat: add commands and queries to products

---
+++ CleanArchMVC/CleanArchMvc.Application/Products/Queries/GetProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Queries
+{
+    public class GetProductQuery: IRequest<IEnumerable<Product>>
+    {
+    }
+}
